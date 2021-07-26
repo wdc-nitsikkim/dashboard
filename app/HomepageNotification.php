1bc94c98c443441872a317ad8fd697bd53994f79
Ankush Yadav <ankushyadav9302@gmail.com>
2021-07-26 23:06:48 +0530

    [UPDATE] app/HomepageNotification
    uses softDeletes trait

--- app/HomepageNotification.php
+++ app/HomepageNotification.php
@@ -3,7 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class HomepageNotification extends Model {
+    use softDeletes;
+
     protected $table = 'homepage_notifications';
+    protected $dates = ['deleted_at'];
 }
