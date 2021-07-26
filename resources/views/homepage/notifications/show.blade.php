d7055d400a92074694ea894c3d010714549d33dc
Ankush Yadav <ankushyadav9302@gmail.com>
2021-07-27 02:36:08 +0530

    [UPDATE] views/homepage.notifications.show
    adds route links

--- resources/views/homepage/notifications/show.blade.php
+++ resources/views/homepage/notifications/show.blade.php
@@ -10,20 +10,24 @@
                 New
             </button>
             <div class="dropdown-menu dashboard-dropdown dropdown-menu-start mt-2 py-1">
-                <a class="dropdown-item d-flex align-items-center" href="#">
+                <a class="dropdown-item d-flex align-items-center"
+                    href="{{ route('homepage.notification.add', 'announcement') }}">
                     <span class="material-icons">campaign</span>
                     Announcement
                 </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
+                <a class="dropdown-item d-flex align-items-center"
+                    href="{{ route('homepage.notification.add', 'download') }}">
                     <span class="material-icons">download_done</span>
                     Download
                 </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
+                <a class="dropdown-item d-flex align-items-center"
+                    href="{{ route('homepage.notification.add', 'notice') }}">
                     <span class="material-icons">description</span>
                     Notice
                 </a>
                 <div role="separator" class="dropdown-divider my-1"></div>
-                <a class="dropdown-item d-flex align-items-center" href="#">
+                <a class="dropdown-item d-flex align-items-center"
+                    href="{{ route('homepage.notification.add', 'tender') }}">
                     <span class="material-icons">apartment</span>
                     Tender
                 </a>
@@ -41,7 +45,6 @@
         <div>
             <a href="#!" class="btn btn-outline-gray-600 d-inline-flex align-items-center">
                 <span class="material-icons mx-1">help</span>
-                Useful Btn
             </a>
         </div>
     </div>
@@ -104,7 +107,7 @@
                                     $query_param = 'enable';
                                 }
                             @endphp
-                            <a href="{{ route('homepage.notification.changeStatus', ['notification'=> $notice['id'], 'status'=> $query_param]) }}"
+                            <a href="{{ route('homepage.notification.changeStatus', ['id'=> $notice['id'], 'status'=> $query_param]) }}"
                                 class="btn btn-xs {{ $btn_class }}">
                                 {{ $btn_text }}
                             </a>
@@ -115,12 +118,16 @@
                                 href="{{ route('homepage.notification.editPage', $notice['id']) }}">
                                 <span class="material-icons">edit</span></a>
                             <a class="text-danger mx-1" data-bs-toggle="tooltip" title="Delete"
-                                href=""><span class="material-icons">delete</span></a>
+                                href="{{ route('homepage.notification.softDelete', $notice['id']) }}"
+                                alert-title="Move to Trash?" alert-text="-"
+                                confirm><span class="material-icons">delete</span></a>
                             @else
                             <a class="text-success mx-1" data-bs-toggle="tooltip" title="Restore"
-                                href=""><span class="material-icons">restore</span></a>
+                                href="{{ route('homepage.notification.restore', $notice['id']) }}">
+                                <span class="material-icons">restore</span></a>
                             <a class="text-danger mx-1" data-bs-toggle="tooltip" title="Delete Permanently"
-                                href=""><span class="material-icons">delete_forever</span></a>
+                                href="{{ route('homepage.notification.delete', $notice['id']) }}"
+                                alert-title="Delete Permanently?" confirm><span class="material-icons">delete_forever</span></a>
                             @endif
                         </td>
                     </tr>
@@ -137,3 +144,7 @@
     </div>
 </div>
 @endsection
+
+@push('scripts')
+    <script src="{{ asset('static/js/homepage-notification.js') }}"></script>
+@endpush
