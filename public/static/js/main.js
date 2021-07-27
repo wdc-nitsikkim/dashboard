0d4cd06841eb9696336c24abe49123a716460894
Ankush Yadav <ankushyadav9302@gmail.com>
2021-07-27 10:10:16 +0530

    [UPDATE] public/static/js/main.js
    adds code to add active classto current menu item (sidenav)

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -1,4 +1,11 @@
 const globalHandler = (function ($, window) {
+    const breakPoints = {
+        sm: 540,
+        md: 720,
+        lg: 960,
+        xl: 1140
+    };
+
     $.ajaxSetup({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -24,5 +31,24 @@ const globalHandler = (function ($, window) {
                 window.location.href = btn.attr('href');
             }
         });
+    });
+
+    function modifySideNav() {
+        const active = $('li.nav-item.active').first();
+        let subMenu = active.closest('div.multi-level.collapse');
+        if (subMenu.length > 0) {
+            const id = subMenu.attr('id');
+            const menuBtn = $(`[data-bs-target='#${id}']`);
+            menuBtn.trigger('click');
+            console.log('Sidenav triggered!');
+            return true;
+        }
+
+        console.log('Sidenav trigger not required!');
+        return false;
+    }
+
+    jQuery(function() {
+        $(window).width() >= breakPoints.lg ? modifySideNav() : console.log('Sidenav trigger cancelled!');
     });
 })(jQuery, window);
