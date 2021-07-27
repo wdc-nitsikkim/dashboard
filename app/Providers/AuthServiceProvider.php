df4d785476a52453a48750c22b17e1f3abc9ba9c
Ankush Yadav <ankushyadav9302@gmail.com>
2021-07-27 19:59:04 +0530

    [ADD] Policies/Homepage/NotificationPolicy, [UPDATE] Providers/AuthServiceProvider
    adds noew policy for HomepageNotification model

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,9 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
+use App\HomepageNotification;
+use App\Policies\Homepage\NotificationPolicy;
+
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -13,7 +16,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        HomepageNotification::class => NotificationPolicy::class
     ];
 
     /**
