69f6f9d491192b6c8c8dbd892983b77e95ee323e
Ankush Yadav <ankushyadav9302@gmail.com>
2021-07-28 15:45:11 +0530

    [REFACTOR] views/includes.{nav, sidenav}

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -119,7 +119,7 @@
                             <img class="avatar rounded-circle" alt="Image placeholder"
                                 src="{{ asset('static/images/admin.webp') }}">
                             <div class="media-body ms-2 text-dark align-items-center d-none d-lg-block">
-                                <span class="mb-0 font-small fw-bold text-gray-900">{{ Auth::user()->name }}</span>
+                                <span class="mb-0 font-small fw-bold text-gray-900">{{ Auth::user()->name ?? '-' }}</span>
                             </div>
                         </div>
                     </a>
--- resources/views/includes/sidenav.blade.php
+++ resources/views/includes/sidenav.blade.php
@@ -24,7 +24,7 @@
                         class="card-img-top rounded-circle border-white" alt="Bonnie Green">
                 </div>
                 <div class="d-block">
-                    <h2 class="h5 mb-3">Hi, {{ Auth::user()->name }}</h2>
+                    <h2 class="h5 mb-3">Hi, {{ Auth::user()->name ?? '-' }}</h2>
                     <a href="{{ route('logout') }}"
                         class="btn btn-secondary btn-sm d-inline-flex align-items-center">
                         <svg class="icon icon-xxs me-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"
@@ -146,8 +146,9 @@
                 </span>
                 <div class="multi-level collapse " role="list" id="submenu-department" aria-expanded="false">
                     <ul class="flex-column nav">
-                        <li class="nav-item {{ Route::is('department.home') ? 'active' : '' }}">
-                            <a class="nav-link" href="{{ route('department.home') }}">
+                        <li class="nav-item
+                            {{ Route::is('department.home') || Route::is('department.select') ? 'active' : '' }}">
+                            <a class="nav-link" href="{{ route('department.index') }}">
                                 <span class="sidebar-text">Home</span>
                             </a>
                         </li>
