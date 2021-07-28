9d5636036eb16b3b747248fa94b0da912d2ea0f4
Ankush Yadav <ankushyadav9302@gmail.com>
2021-07-28 12:19:22 +0530

    [UPDATE] Models/Department, Providers/RouteServiceProvider
    explicitly binds 'code' attribute in routes to \App\Models\Department, update default column for lookup

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -5,5 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model {
+    protected $table = 'departments';
 
+    /* match route parameter to this value instead of 'id' */
+    public function getRouteKeyName() {
+        return 'code';
+    }
 }
