2f0dc2e4d8e0e28334dea957ffbff321b6cc3af8
Ankush Yadav <ankushyadav9302@gmail.com>
2021-07-28 23:29:40 +0530

    [UPDATE] migrations/..users_table..
    adds 'root' role to enum list

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->enum('role',
-                ['admin', 'hod', 'faculty', 'student', 'office', 'tnp', 'ecell', 'other'])
+                ['root', 'admin', 'hod', 'faculty', 'student', 'office', 'tnp', 'ecell', 'other'])
                 ->default('other');
             $table->string('name');
             $table->string('email')->unique();
