1d2fa9c1c97026fad97a114b78213b61c1cb3e60
Ankush Yadav <ankushyadav9302@gmail.com>
2021-07-29 03:49:55 +0530

    [FIX] Policies/DepartmentPolicy
    fixes incorrect application, usage

--- app/Policies/DepartmentPolicy.php
+++ app/Policies/DepartmentPolicy.php
@@ -4,6 +4,7 @@ namespace App\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
 
+use Auth;
 use App\Models\User;
 use App\CustomHelper;
 use App\Models\Department;
@@ -15,34 +16,31 @@ class DepartmentPolicy
     /* TODO: to be made dynamic */
     protected $view_roles = ['admin', 'office', 'hod', 'ecell', 'faculty'];
     protected $create_roles = ['admin'];
-    protected $update_roles = ['admin', 'office', 'hod'];
+    protected $update_roles = ['admin', 'office'];
     protected $delete_roles = ['admin'];
 
     public function __construct() {
         $this->permission = CustomHelper::get_permission_constants();
+        $this->user_permissions = Auth::user()->permissions->pluck('permission')->toArray();
     }
 
     public function view(User $user) {
-        $user_permissions = $user->permissions->pluck('permission')->toArray();
         return \in_array($user->role, $this->view_roles)
-            && \in_array($this->permission['read'], $user_permissions);
+            && \in_array($this->permission['read'], $this->user_permissions);
     }
 
     public function create(User $user) {
-        $user_permissions = $user->permissions->pluck('permission')->toArray();
         return \in_array($user->role, $this->create_roles)
-            && \in_array($this->permission['write'], $user_permissions);
+            && \in_array($this->permission['write'], $this->user_permissions);
     }
 
     public function update(User $user) {
-        $user_permissions = $user->permissions->pluck('permission')->toArray();
         return \in_array($user->role, $this->update_roles)
-            && \in_array($this->permission['write'], $user_permissions);
+            && \in_array($this->permission['write'], $this->user_permissions);
     }
 
     public function delete(User $user) {
-        $user_permissions = $user->permissions->pluck('permission')->toArray();
         return \in_array($user->role, $this->delete_roles)
-            && \in_array($this->permission['delete'], $user_permissions);
+            && \in_array($this->permission['delete'], $this->user_permissions);
     }
 }
