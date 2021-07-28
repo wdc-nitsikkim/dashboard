34a06c496faab0e8b50da8f1a5bf365fb829112e
Ankush Yadav <ankushyadav9302@gmail.com>
2021-07-29 03:41:37 +0530

    [REFACTOR] Models/User
    changes dynamic property name

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,7 +37,7 @@ class User extends Authenticatable
         return $this->hasMany(UserPermission::class, 'user_id');
     }
 
-    public function departments() {
+    public function allowed_departments() {
         return $this->hasMany(UserAccessDepartment::class, 'user_id');
     }
 }
