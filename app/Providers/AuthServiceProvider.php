19050cb15cca6dde6304b2ff7c6b9c9225979426
Ankush Yadav <ankushyadav9302@gmail.com>
2021-07-30 02:42:44 +0530

    [ADD] Policies/StudentPolicy, [UPDATE] Providers/AuthServiceProvider
    adds student policy and registers it

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,9 +5,11 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
+use App\Models\Student;
 use App\Models\Department;
 use App\Models\HomepageNotification;
 use App\Policies\DepartmentPolicy;
+use App\Policies\StudentPolicy;
 use App\Policies\HomepageNotificationPolicy as NotificationPolicy;
 
 class AuthServiceProvider extends ServiceProvider
@@ -19,7 +21,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         HomepageNotification::class => NotificationPolicy::class,
-        Department::class => DepartmentPolicy::class
+        Department::class => DepartmentPolicy::class,
+        Student::class => StudentPolicy::class
     ];
 
     /**
@@ -31,8 +34,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
+        /* all actions allowed for 'root' user */
         Gate::before(function($user, $ability) {
-            return $user->role === 'root' ? true : null;
+            return $user->hasRole('root') ? true : null;
         });
     }
 }
