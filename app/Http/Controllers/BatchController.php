56c52c911629efa22b600799a480981a1fcdc0a6
Ankush Yadav <ankushyadav9302@gmail.com>
2021-08-02 21:00:03 +0530

    [FEATURE] Adds page to view batch list
    -> Uses additional pagination methods to control multiple paginator instances on same page

--- app/Http/Controllers/BatchController.php
+++ app/Http/Controllers/BatchController.php
@@ -10,6 +10,13 @@ use App\Models\Batch;
 
 class BatchController extends Controller {
     /**
+     * Items per page
+     *
+     * @var int
+     */
+    private $paginate = 5;
+
+    /**
      * Stores session keys received from \CustomHelper::getSessionConstants()
      *
      * @var null|array
@@ -36,11 +43,29 @@ class BatchController extends Controller {
 
         return Route::has($redirectRouteName)
             ? redirect()->route($redirectRouteName, $batch)
-            : redirect()->route('batch.show', $batch);
+            : redirect()->route('batch.show');
     }
 
-    public function show() {
+    public function show(Request $request) {
+        $btechPage = $request->btech;
+        $mtechPage = $request->mtech;
 
+        $btechBatches = Batch::where('type', 'b')->orderByDesc('id')
+            ->paginate($this->paginate, ['*'], 'btech', $btechPage);
+        $mtechBatches = Batch::where('type', 'm')->orderByDesc('id')
+            ->paginate($this->paginate, ['*'], 'mtech', $mtechPage);
+
+        $btechBatches->setPageName('btech');
+        $btechBatches->appends(['mtech' => $mtechPage]);
+        $mtechBatches->setPageName('mtech');
+        $mtechBatches->appends(['btech' => $btechPage]);
+
+        return view('batch.show', [
+            'btechBatches' => $btechBatches->toArray(),
+            'btechPagination' => $btechBatches->links('vendor.pagination.default'),
+            'mtechBatches' => $mtechBatches->toArray(),
+            'mtechPagination' => $mtechBatches->links('vendor.pagination.default')
+        ]);
     }
 
     public function test() {
