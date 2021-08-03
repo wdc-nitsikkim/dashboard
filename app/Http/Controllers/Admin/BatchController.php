30ed553c6ef2e8941b34b11259ea467fc32d43ab
Ankush Yadav <ankushyadav9302@gmail.com>
2021-08-04 01:42:38 +0530

    [FIX] Add missing dependency

--- app/Http/Controllers/Admin/BatchController.php
+++ app/Http/Controllers/Admin/BatchController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
 use App\CustomHelper;
