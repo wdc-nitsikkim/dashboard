c4ce58111208867ee55215852798715b47c70ce0
Ankush Yadav <ankushyadav9302@gmail.com>
2021-08-03 20:45:22 +0530

    [UPDATE] Add simple back anchor link component
    -> Use case: in paginated pages when no results are found

---
+++ resources/views/components/inline/anchorBack.blade.php
@@ -0,0 +1,9 @@
+{{-- Identical to componenets.inline.anchorLink. Made separate because of
+    this is specifically for back buttons (pagination -- no results found page) --}}
+
+<a class="{{ $classes ?? 'text-info' }}" href="{{ $href ?? '#!' }}">
+    <span class="material-icons">{{ $icon ?? 'keyboard_arrow_left'}}</span>
+    @isset($slot)
+        {{ $slot == '' ? 'Go Back' : $slot }}
+    @endisset
+</a>
