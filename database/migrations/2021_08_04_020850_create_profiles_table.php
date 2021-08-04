9619a631bf7d4bd1746bb6fee82c4f8d4b9fe1d0
Ankush Yadav <ankushyadav9302@gmail.com>
2021-08-04 08:46:38 +0530

    [UPDATE] Add migrations for 'profiles', 'user_profile_links' tables

---
+++ database/migrations/2021_08_04_020850_create_profiles_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type')->nullable(false);
+            $table->unsignedSmallInteger('department_id');
+            $table->string('name')->nullable(false);
+            $table->string('mobile')->nullable(false);
+            $table->string('email')->nullable(false);
+            $table->string('designation');
+            $table->text('academic_qualifications');
+            $table->text('areas_of_interest');
+            $table->text('work_experience');
+            $table->text('teachings');
+            $table->text('office_address');
+            $table->longText('publications');
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->unique('email');
+            $table->unique('mobile');
+            $table->foreign('department_id')->references('id')->on('departments')
+                ->onUpdate('cascade')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
---
+++ database/migrations/2021_08_04_020913_create_user_profile_links_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserProfileLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profile_links', function (Blueprint $table) {
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('profile_id');
+
+            $table->unique('user_id');
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('profile_id')->references('id')->on('profiles')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profile_links');
+    }
+}
