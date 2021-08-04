ee87a5bc68f226ac66ea23278f1ebf52f4589e67
Ankush Yadav <ankushyadav9302@gmail.com>
2021-08-04 23:39:12 +0530

    [STYLE] Fix typo in comment

--- resources/views/components/inline/anchorBack.blade.php
+++ resources/views/components/inline/anchorBack.blade.php
@@ -1,5 +1,5 @@
-{{-- Identical to componenets.inline.anchorLink. Made separate because of
-    this is specifically for back buttons (pagination -- no results found page) --}}
+{{-- Identical to componenets.inline.anchorLink. Made separate because
+    this is specifically for back buttons (pagination -> no results found page) --}}
 
 <a class="{{ $classes ?? 'text-info' }}" href="{{ $href ?? '#!' }}">
     <span class="material-icons">{{ $icon ?? 'keyboard_arrow_left'}}</span>
