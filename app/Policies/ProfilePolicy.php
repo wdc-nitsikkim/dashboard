01f8aecaae22a1e8f1483aa7f5182a298700464c
Ankush Yadav <ankushyadav9302@gmail.com>
2021-08-05 03:35:05 +0530

    [FEATURE] Create profile policy & register it in AuthServiceProvider

---
+++ app/Policies/ProfilePolicy.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+use App\Models\User;
+use App\CustomHelper;
+use App\Models\Profile;
+use App\Models\UserProfileLink;
+
+class ProfilePolicy {
+    use HandlesAuthorization;
+
+    /**
+     * Valid role list for specified access
+     *
+     * @var array
+     */
+    protected $view_roles = ['admin', 'office', 'hod', 'ecell', 'faculty', 'tnp'];
+    protected $create_roles = ['admin'];
+    protected $update_roles = ['admin'];
+    protected $delete_roles = ['admin'];
+
+    public function __construct() {
+        $this->permission = CustomHelper::getPermissionConstants();
+    }
+
+    /**
+     * @param App\Models\User $user
+     */
+    public function view(User $user) {
+        return $user->isPermissionValid($this->view_roles, $this->permission['read']);
+    }
+
+    /**
+     * Whether user is authorized to create profiles
+     * Users with roles 'hod', 'faculty', 'staff' can create a single profile only
+     * which belongs to them
+     *
+     * @param App\Models\User $user
+     * @return boolean
+     */
+    public function create(User $user) {
+        if ($user->hasRole('hod', 'faculty', 'staff')) {
+            return is_null($user->profileLink);
+        }
+        return $user->isPermissionValid($this->create_roles, $this->permission['create']);
+    }
+
+    /**
+     * Whether user is authorized to update profiles
+     * * Users with roles 'hod', 'faculty', 'staff' can only update thier own profiles
+     *
+     * @param App\Models\User $user
+     * @param App\Models\Profile $profile
+     * @return boolean
+     */
+    public function update(User $user, Profile $profile) {
+        if ($user->hasRole('hod', 'faculty', 'staff') && $user->hasProfile()) {
+            return $user->profileLink->profile_id === $profile->id;
+        }
+
+        return $user->isPermissionValid($this->update_roles, $this->permission['update']);
+    }
+
+    public function delete(User $user, Profile $profile) {
+        return $user->isPermissionValid($this->delete_roles, $this->permission['delete']);
+    }
+
+    public function chooseType(User $user) {
+        return $user->hasRole('admin');
+    }
+
+    public function customizeLinkOption(User $user) {
+        return $user->hasRole('admin');
+    }
+}
