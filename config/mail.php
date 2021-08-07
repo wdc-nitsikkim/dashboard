20f067bc83353dcb6d8a55404b4912abd421d9cc
Ankush Yadav <ankushyadav9302@gmail.com>
2021-08-08 03:22:30 +0530

    [FEATURE] Configure gmail SMTP

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'wdc@nitsikkim.ac.in'),
+        'name' => env('MAIL_FROM_NAME', 'WDC, NIT Sikkim'),
     ],
 
     /*
