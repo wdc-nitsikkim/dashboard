826d0e5e59a496437a5d0bad514f7734f6a0bd16
Ankush Yadav <ankushyadav9302@gmail.com>
2021-08-07 12:43:19 +0530

    [UPDATE] Add function to store uploaded images in 'StoreFiles' trait

--- app/Traits/StoreFiles.php
+++ app/Traits/StoreFiles.php
@@ -11,7 +11,8 @@ use App\CustomHelper;
  * Class contaning pre-defined functions to store files at proper locations
  */
 trait StoreFiles {
-    protected $notificationBasePath = 'files/homepage/';
+    protected $notificationBasePath = 'files/homepage';
+    protected $profileImageBasePath = 'images/profiles';
 
     /**
      * Handles homepage notification attachments (always saved as public files)
@@ -19,15 +20,29 @@ trait StoreFiles {
      * @param Illuminate\Http\UploadedFile $file  File to be saved
      * @param string $type  (optional)
      * @param boolean $absolute  (optional) Whether to return absolute path
-     * @return string  File path of saved file
+     * @return string  Path of saved file
      */
     public function storeNotification($file, $type = 'others', bool $absolute = true) {
-        $file = request()->file('attachment');
         $fileName = CustomHelper::formatFileName($file->getClientOriginalName());
         $fileName .= '.' . $file->extension();
-        $storagePath = $this->notificationBasePath . $type;
+        $storagePath = $this->notificationBasePath . '/' . $type;
         $path = $file->storeAs($storagePath, $fileName, 'public');
 
         return $absolute ? asset(Storage::url($path)) : Storage::url($path);
+    }
+
+    /**
+     * Handles uploading of profile images
+     *
+     * @param Illuminate\Http\UploadedFile $image  Image to be uploaded
+     * @param int $profile_id
+     * @return string  Path of saved image
+     */
+    public function storeProfileImage($image, $profile_id) {
+        $fileName = $profile_id . '.' . $image->extension();
+        $storagePath = $this->profileImageBasePath;
+        $path = $image->storeAs($storagePath, $fileName, 'public');
+
+        return $path;
     }
 }
