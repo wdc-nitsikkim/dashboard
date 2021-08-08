17e0cdcb47220ab1099babebc96f021be50012e1
Ankush Yadav <ankushyadav9302@gmail.com>
2021-08-09 04:00:14 +0530

    [FEATURE] Enable signin via google

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Validator;
+use Google_Client;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,13 +14,6 @@ use App\CustomHelper;
 use App\Models\User;
 
 class LoginController extends Controller {
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
     /**
      * Create a new controller instance.
      *
@@ -33,7 +29,84 @@ class LoginController extends Controller {
     }
 
     public function withGoogle(Request $request) {
-        dd($request->all());
+        $cookieVal = $_COOKIE['g_csrf_token'] ?? '';
+        $validator = Validator::make($request->post(),[
+            'g_csrf_token' => ['required', 'string', 'in:' . $cookieVal],
+            'credential' => 'required | string'
+        ]);
+
+        if ($validator->fails()) {
+            return back()->with([
+                'status' => 'fail',
+                'message' => 'Double submit cookie verification failed!'
+            ]);
+        }
+
+        $validIssuers = ['https://accounts.google.com', 'accounts.google.com'];
+        $validDomains = ['nitsikkim.ac.in'];
+        $clientId = config('app.g_signin_client_id');
+        $gClient = new Google_Client([ 'client_id' => $clientId ]);
+
+        try {
+            $payload = $gClient->verifyIdToken($request->post('credential'));
+        } catch (\Exception $e) {
+            return back()->with([
+                'status' => 'fail',
+                'message' => $e->getMessage()
+            ]);
+        }
+
+        if (!$payload) {
+            return back()->with([
+                'status' => 'fail',
+                'message' => 'Auth token invalid!'
+            ]);
+        }
+
+        $tokenIssuer = $payload['iss'] ?? '';
+        $tokenHdClaim = $payload['hd'] ?? '';
+
+        if (!in_array($tokenIssuer, $validIssuers)) {
+            return back()->with([
+                'status' => 'fail',
+                'message' => 'Auth server did not respond correctly!'
+            ]);
+        }
+        if (!in_array($tokenHdClaim, $validDomains)) {
+            return back()->with([
+                'status' => 'fail',
+                'message' => 'Organization error!'
+            ]);
+        }
+        if (empty($payload['email']) || empty($payload['name'])) {
+            return back()->with([
+                'status' => 'fail',
+                'message' => 'Unable to access user profile'
+            ]);
+        }
+
+        $email = $payload['email'];
+        $user = User::where('email', $email)->first();
+
+        if (is_null($user)) {
+            return $this->redirectRegister($payload);
+        }
+
+        Auth::loginUsingId($user->id);
+        return redirect()->route('root.default')->with([
+            'status' => 'success',
+            'message' => 'Signed in with Google'
+        ]);
+    }
+
+    public function redirectRegister($data = null) {
+        return redirect()->route('register')->withInput([
+            'name' => $data['name'] ?? '',
+            'email' => $data['email'] ?? ''
+        ])->with([
+            'status' => 'info',
+            'message' => 'No account found'
+        ]);
     }
 
     public function test() {
