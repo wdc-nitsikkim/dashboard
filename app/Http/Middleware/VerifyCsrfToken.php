f811192206ae473b4f9e60902d27cb194fe48e6a
Ankush Yadav <ankushyadav9302@gmail.com>
2021-08-09 08:29:27 +0530

    [REFACTOR] Remove unused route from middleware, add 'mobile' as fillable property in User model

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'auth/signin/google',
-        'auth/signup/google'
+        'auth/signin/google'
     ];
 }
