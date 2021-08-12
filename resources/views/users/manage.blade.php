8ebfa4c461a8e7bfab821e7ad19d986d61e50eb0
Ankush Yadav <ankushyadav9302@gmail.com>
2021-08-12 11:40:06 +0530

    [REFACTOR] Improve UX

--- resources/views/users/manage.blade.php
+++ resources/views/users/manage.blade.php
@@ -6,10 +6,6 @@
 
 @section('content')
 
-@php
-
-@endphp
-
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center mt-3">
     <div>
         <div class="dropdown">
@@ -51,78 +47,124 @@
     @endslot
 @endcomponent
 
-<div class="card border-0 shadow mb-4">
-    <div class="card-body">
+<div class="row">
+    <div class="col-12">
+        <div class="card border-0 shadow mb-4">
+            <div class="card-body">
+                <h5 class="mb-2">Roles & Permissions</h5>
 
-        @if ($user->roles->count() == 0)
-            <h5 class="text-center text-danger">This user has no roles assigned!</h5>
-            <p class="text-center">
-                @component('components.inline.anchorBack', [
-                    'href' => route('users.account', $user->id)
-                ])
-                @endcomponent
-            </p>
-        @else
-            <form method="POST" action="{{ route('users.savePermissions', $user->id) }}">
-                {{ csrf_field() }}
+                @if ($user->roles->count() == 0)
+                    <p class="text-danger">No roles have been assigned to this account!</p>
+                @else
+                    <form method="POST" action="{{ route('users.savePermissions', $user->id) }}">
+                        {{ csrf_field() }}
 
-                @component('components.table.main')
-                    @slot('head')
-                        @component('components.table.head', [
-                            'items' => [
-                                '#', 'Role', 'Create', 'Read', 'Update', 'Delete',
-                                'Granted On'
-                            ]
-                        ])
+                        @component('components.table.main')
+                            @slot('head')
+                                @component('components.table.head', [
+                                    'items' => [
+                                        '#', 'Role', 'Create', 'Read', 'Update', 'Delete',
+                                        'Granted On'
+                                    ]
+                                ])
+                                @endcomponent
+                            @endslot
+
+                            @slot('body')
+                                @foreach ($user->roles as $role)
+                                    <tr>
+                                        <td class="fw-bolder">{{ $loop->iteration }}</td>
+                                        <td class="fw-bolder">{{ ucfirst($role->role) }}</td>
+                                        <td>
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" value="grant"
+                                                    name="role[{{ $role->role }}][c]"
+                                                    {{ $role->permissions->contains('permission', 'c') ? 'checked' : '' }}>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" value="grant"
+                                                    name="role[{{ $role->role }}][r]"
+                                                    {{ $role->permissions->contains('permission', 'r') ? 'checked' : '' }}>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" value="grant"
+                                                    name="role[{{ $role->role }}][u]"
+                                                    {{ $role->permissions->contains('permission', 'u') ? 'checked' : '' }}>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" value="grant"
+                                                    name="role[{{ $role->role }}][d]"
+                                                    {{ $role->permissions->contains('permission', 'd') ? 'checked' : '' }}>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            {{ $role->created_at }}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @endslot
                         @endcomponent
-                    @endslot
 
-                    @slot('body')
-                        @foreach ($user->roles as $role)
-                            <tr>
-                                <td class="fw-bolder">{{ $loop->iteration }}</td>
-                                <td class="fw-bolder">{{ ucfirst($role->role) }}</td>
-                                <td>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="grant"
-                                            name="role[{{ $role->role }}][c]"
-                                            {{ $role->permissions->contains('permission', 'c') ? 'checked' : '' }}>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="grant"
-                                            name="role[{{ $role->role }}][r]"
-                                            {{ $role->permissions->contains('permission', 'r') ? 'checked' : '' }}>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="grant"
-                                            name="role[{{ $role->role }}][u]"
-                                            {{ $role->permissions->contains('permission', 'u') ? 'checked' : '' }}>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="grant"
-                                            name="role[{{ $role->role }}][d]"
-                                            {{ $role->permissions->contains('permission', 'd') ? 'checked' : '' }}>
-                                    </div>
-                                </td>
-                                <td>
-                                    {{ $role->created_at }}
-                                </td>
-                            </tr>
-                        @endforeach
-                    @endslot
-                @endcomponent
+                        <button class="btn btn-primary animate-up-2 mt-2" type="submit" confirm
+                            alert-title="Save these permissions?" alert-text="-" alert-timer="5000">
+                            Save</button>
+                    </form>
 
-                <button class="btn btn-primary animate-up-2 mt-2" type="submit">Save</button>
-            </form>
+                @endif
 
-        @endif
+            </div>
+        </div>
+    </div>
 
+    <div class="col-12 col-md-6">
+        <div class="card border-0 shadow mb-4">
+            <div class="card-body">
+                <h5 class="mb-2">Department Access</h5>
+
+                @if ($user->roles->count() == 0)
+                    <p class="text-danger">No Results / Not Applicable</p>
+                @else
+                    @component('components.table.main')
+                        @slot('head')
+                            @component('components.table.head', [
+                                'items' => [
+                                    '#', 'Name', 'Granted on'
+                                ]
+                            ])
+                            @endcomponent
+                        @endslot
+
+                        @slot('body')
+                            @foreach ($user->allowedDepartments as $dept)
+                                <tr>
+                                    <td class="fw-bolder">{{ $loop->iteration }}</td>
+                                    <td class="fw-bolder">{{ $dept->department->name }}</td>
+                                    <td>
+                                        {{ $dept->created_at }}
+                                    </td>
+                                </tr>
+                            @endforeach
+                        @endslot
+                    @endcomponent
+                @endif
+
+            </div>
+        </div>
+    </div>
+
+    <div class="col-12 col-md-6">
+        <div class="card border-0 shadow mb-4">
+            <div class="card-body">
+                <h5 class="mb-2">Subject Access</h5>
+                <p class="text-info">-> Feature under development</p>
+            </div>
+        </div>
     </div>
 </div>
 
