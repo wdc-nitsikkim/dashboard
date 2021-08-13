315a546c66b7d5c9eb0bc40ce05ad0112d32abc1
Ankush Yadav <ankushyadav9302@gmail.com>
2021-08-13 12:55:24 +0530

    [FEATURE] Add option to backup & download database

--- resources/views/manageSite.blade.php
+++ resources/views/manageSite.blade.php
@@ -26,7 +26,7 @@
 
             @component('components.inline.anchorBtn', [
                 'href' => route('artisan.command', 'cache:clear'),
-                'classes' => 'btn-lg btn-outline-tertiary mb-2'
+                'classes' => 'btn-lg btn-outline-info mb-2'
             ])
                 @slot('attr')
                     spoof spoof-method="POST"
@@ -36,7 +36,7 @@
 
             @component('components.inline.anchorBtn', [
                 'href' => route('artisan.command', 'config:clear'),
-                'classes' => 'btn-lg btn-outline-tertiary mb-2'
+                'classes' => 'btn-lg btn-outline-danger mb-2'
             ])
                 @slot('attr')
                     spoof spoof-method="POST"
@@ -70,36 +70,63 @@
 
             @component('components.inline.anchorBtn', [
                 'href' => route('artisan.command', 'config:cache'),
-                'classes' => 'btn-lg btn-outline-tertiary mb-2'
+                'classes' => 'btn-lg btn-outline-danger mb-2'
             ])
                 @slot('attr')
-                    spoof spoof-method="POST"
+                    spoof spoof-method="POST" confirm
+                    alert-title="Rebuild Cache?"
+                    alert-text="Old cache will be purged"
                 @endslot
                 Rebuild Config Cache
             @endcomponent
 
             @component('components.inline.anchorBtn', [
-                'href' => route('artisan.command', 'route:cache'),
-                'classes' => 'btn-lg btn-outline-tertiary mb-2'
+                'href' => '#!',
+                'classes' => 'btn-lg btn-outline-tertiary mb-2 disabled'
             ])
-                @slot('attr')
-                    spoof spoof-method="POST"
-                @endslot
                 Cache Routes
             @endcomponent
-
-        </div>
-
-        <div class="mb-3">
 
             @component('components.inline.anchorBtn', [
                 'href' => route('artisan.command', 'storage:link'),
                 'classes' => 'btn-lg btn-outline-tertiary mb-2'
             ])
                 @slot('attr')
-                    spoof spoof-method="POST"
+                    spoof spoof-method="POST" confirm
+                    alert-title="Sure to create a symlink?"
+                    alert-text="-"
                 @endslot
                 Link Storage
+            @endcomponent
+
+        </div>
+
+        <h5 class="mb-3">Backup</h5>
+
+        <div class="mb-3">
+
+            @component('components.inline.anchorBtn', [
+                'href' => route('dbBackupCreate'),
+                'classes' => 'btn-lg btn-outline-success mb-2'
+            ])
+                @slot('attr')
+                    spoof spoof-method="POST" confirm
+                    alert-title="Create a backup now?"
+                    alert-text="It will be downloaded automatically & will be
+                    stored in the server filesystem"
+                @endslot
+                Database Backup
+            @endcomponent
+
+            @component('components.inline.anchorBtn', [
+                'href' => route('removeBackupDir'),
+                'classes' => 'btn-lg btn-outline-danger mb-2'
+            ])
+                @slot('attr')
+                    spoof spoof-method="POST" confirm
+                    alert-title="Destructive Action!" alert-timer="2000"
+                @endslot
+                Remove all backups
             @endcomponent
 
         </div>
