23d6c77e93e36beef12a9837ddb8fa6af2d7715e
Ankush Yadav <ankushyadav9302@gmail.com>
2021-08-13 12:51:14 +0530

    [UPDATE] Add useful methods to StoreFiles trait

--- app/Traits/StoreFiles.php
+++ app/Traits/StoreFiles.php
@@ -2,6 +2,8 @@
 
 namespace App\Traits;
 
+use File;
+
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
@@ -14,6 +16,7 @@ trait StoreFiles {
     protected $notificationBasePath = 'files/homepage';
     protected $profileImageBasePath = 'images/profiles';
     protected $userImageBasePath = 'images/users';
+    protected $backupDir = 'backup';
 
     /**
      * Handles homepage notification attachments (always saved as public files)
@@ -72,6 +75,51 @@ trait StoreFiles {
         try {
             return Storage::disk('public')->delete($path);
         } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    /**
+     * Permanently delete a directory
+     *
+     * @param string $path  Relative to 'local' disk
+     */
+    public function removeDirectory($path) {
+        try {
+            return Storage::disk('local')->deleteDirectory($path);
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    /**
+     * Create backup directory if not exists
+     */
+    public function createBackupDir() {
+        if (! File::isDirectory(config('filesystems.disks.local.root')
+            . '/' . $this->backupDir)) {
+
+            Storage::disk('local')->makeDirectory($this->backupDir);
+        }
+    }
+
+    /**
+     * Move file from absolute path to local disk
+     *
+     * @param string $absolutePath  Absolute path offile to be moved
+     * @param string $fileName  Name of file in the local disk
+     * @param boolean $remove  (optional) Whether to delete file if error occurs
+     */
+    public function moveFileToLocalDisk($absolutePath, $fileName, $remove = true) {
+        $relativePath = $this->backupDir . '/' . $fileName;
+        $dest = config('filesystems.disks.local.root') . '/'
+            . $relativePath;
+
+        try {
+            File::move($absolutePath, $dest);
+            return $dest;
+        } catch (\Exception $e) {
+            $remove ? File::delete($absolutePath) : false;
             return false;
         }
     }
