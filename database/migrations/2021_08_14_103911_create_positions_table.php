1071d43cf3771adedd83cf49e9228791af6e56ed
Ankush Yadav <ankushyadav9302@gmail.com>
2021-08-14 16:21:30 +0530

    [UPDATE] Add Position model & migration

---
+++ database/migrations/2021_08_14_103911_create_positions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePositionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('positions', function (Blueprint $table) {
+            $table->unsignedInteger('profile_id');
+            $table->string('position')->nullable(false);
+            $table->string('mobile')->nullable(false);
+            $table->string('email')->nullable(false);
+
+            $table->foreign('profile_id')->references('id')->on('profiles')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('positions');
+    }
+}
