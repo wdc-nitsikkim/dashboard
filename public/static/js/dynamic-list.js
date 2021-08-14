91b3392cfe25f97ff5782c5b488a86f057dd7a40
Ankush Yadav <ankushyadav9302@gmail.com>
2021-08-14 09:46:54 +0530

    [UPDATE] Optimize search feature, reduce no. of requests sent to server

--- public/static/js/dynamic-list.js
+++ public/static/js/dynamic-list.js
@@ -1,6 +1,12 @@
 const dynamicList = (function ($, window, main) {
     'use strict';
 
+    let listAjax = undefined;
+    let listTimer = undefined;
+
+    /* wait for given gap between keyboard strokes before sending request */
+    const typegap = 750;
+
     function getListItem(item, radioName, autofill) {
         return `<label class='list-group-item'>
             <input class='form-check-input me-1' type='radio' name='${radioName}'
@@ -10,12 +16,17 @@ const dynamicList = (function ($, window, main) {
         </label>`;
     }
 
-    $('input[dynamic-list]').on('input', function(e) {
+    $('input[dynamic-list]').on('input', function () {
         const input = $(this);
         const id = input.attr('dynamic-list');
         const container = $('#' + id);
         const val = input.val()?.trim();
         const defaultText = main.getSpanMsg('Atleast a letter is required');
+
+        clearTimeout(listTimer);
+        if (listAjax) {
+            listAjax.abort();
+        }
 
         if (typeof val == 'undefined' || val == '' || val.length == 0) {
             return main.fillContainer(container, defaultText);
@@ -26,33 +37,35 @@ const dynamicList = (function ($, window, main) {
         const autofill = input.attr('autofill');
         const listRadioName = input.attr('tmp-name');
 
-        $.ajax({
-            url: endpoint,
-            dataType: 'json',
-            beforeSend: () => {
-                loader.removeClass('d-none');
-            }
-        }).done(function(response) {
-            if (response.length == 0) {
-                return main.fillContainer(
-                    container,
-                    main.getSpanMsg('No results!', 'text-danger small')
-                );
-            }
+        listTimer = setTimeout(() => {
+            listAjax = $.ajax({
+                url: endpoint,
+                dataType: 'json',
+                beforeSend: () => {
+                    loader.removeClass('d-none');
+                }
+            }).done(response => {
+                if (response.length == 0) {
+                    return main.fillContainer(
+                        container,
+                        main.getSpanMsg('No results!', 'text-danger small')
+                    );
+                }
 
-            main.fillContainer(container);
+                main.fillContainer(container);
 
-            response.forEach(item => {
-                container.append(getListItem(item, listRadioName, autofill));
+                response.forEach(item => {
+                    container.append(getListItem(item, listRadioName, autofill));
+                });
+            }).fail(() => {
+                console.log('Request failed!');
+            }).always(() => {
+                loader.addClass('d-none');
             });
-        }).fail(function() {
-            console.log('Request failed!');
-        }).always(function() {
-            loader.addClass('d-none');
-        });
+        }, typegap);
     });
 
-    $(window.document).on('click', 'input[fill]', function(e) {
+    $(window.document).on('click', 'input[fill]', function () {
         const input = $(this);
         $(`#${input.attr('fill')}`).val(input.val());
     });
