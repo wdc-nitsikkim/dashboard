2ad19d1b799a19f906f6a4f3d1c958a04c529599
Ankush Yadav <ankushyadav9302@gmail.com>
2021-08-21 02:54:39 +0530

    [UPDATE] Add feedbacks table model & migration

---
+++ app/Models/Feedback.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+use App\Traits\GlobalAccessors;
+
+class Feedback extends Model {
+    use GlobalAccessors;
+
+    protected $table = 'feedbacks';
+
+    /**
+     * The attributes that are mass-assignable
+     */
+    protected $fillable = ['user_id', 'rating', 'feedback'];
+
+    /**
+     * Defines many-to-one relationship
+     */
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id')->withDefault();
+    }
+}
