c81ae6f73322c07420ac83503295e36717cd1af4
Ankush Yadav <ankushyadav9302@gmail.com>
2021-08-22 06:41:41 +0530

    [FEATURE] Add reset password backend

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -2,38 +2,79 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Support\Facades\Hash;
 
-class ResetPasswordController extends Controller
-{
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset requests
-    | and uses a simple trait to include this behavior. You're free to
-    | explore this trait and override any methods you wish to tweak.
-    |
-    */
+use App\CustomHelper;
+use App\Models\User;
+use App\Models\PasswordReset;
 
-    use ResetsPasswords;
-
-    /**
-     * Where to redirect users after resetting their password.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
+class ResetPasswordController extends Controller {
+    public function __construct() {
         $this->middleware('guest');
+    }
+
+    public function show($email, $token) {
+        $dbToken = $this->validateLink($email, $token);
+
+        if ($this->isLinkExpired($dbToken)) {
+            abort(410);
+        }
+
+        return view('auth.resetPassword', [
+            'userInfo' => $dbToken->user->name . ', ' . $email
+        ]);
+    }
+
+    public function reset(Request $request, $email, $token) {
+        $request->validate([
+            'password' => 'required | min:6 | confirmed'
+        ]);
+
+        $dbToken = $this->validateLink($email, $token);
+
+        if ($this->isLinkExpired($dbToken)) {
+            abort(410);
+        }
+
+        try {
+            User::where('id', $dbToken->user->id)
+                ->update([
+                    'password' => Hash::make($request->password)
+                ]);
+            $dbToken->delete();
+        } catch (\Exception $e) {
+            Log::debug('Failed to reset password', [$e->getMessage(), $user, $dbToken]);
+            return back()->with([
+                'status' => 'fail',
+                'message' => 'Failed to update password! Please try again'
+            ]);
+        }
+
+        return redirect()->route('login')->with([
+            'status' => 'success',
+            'message' => 'Password updated succesfully. Login to continue'
+        ]);
+    }
+
+    private function validateLink($email, $token) {
+        if (strlen($token) != CustomHelper::RESET_PWD_TOKEN_LEN) {
+            abort(404);
+        }
+
+        return PasswordReset::with('user:id,email,name')->where([
+            'email' => $email,
+            'token' => $token
+        ])->firstOrFail();
+    }
+
+    private function isLinkExpired($tokenRow) {
+        $validSeconds = 3600;
+
+        $tokenCreationTime = strtotime($tokenRow->created_at);
+        $current = strtotime(now());
+
+        return ($current - $tokenCreationTime) > $validSeconds;
     }
 }
