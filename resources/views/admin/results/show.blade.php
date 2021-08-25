f3bc0d66e400f362d78928e4db2ed0dcb5d096b2
Ankush Yadav <ankushyadav9302@gmail.com>
2021-08-25 12:16:58 +0530

    [REFACTOR] Change dropdown button colors

--- resources/views/admin/results/show.blade.php
+++ resources/views/admin/results/show.blade.php
@@ -37,7 +37,8 @@
         <div>
             <span class="h5 fw-bolder me-3">{{ $currentResultType->name }}</span>
 
-            <button class="btn btn-gray-800 d-inline-flex align-items-center dropdown-toggle mb-2" data-bs-toggle="dropdown">
+            <button class="btn btn-outline-primary d-inline-flex align-items-center dropdown-toggle
+                mb-2" data-bs-toggle="dropdown">
                 Result Type
                 <span class="material-icons ms-1">keyboard_arrow_down</span>
             </button>
