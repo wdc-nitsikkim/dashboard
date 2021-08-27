4c46f07c25fcd3914bd4e07e3c5f0199fb3ab967
Ankush Yadav <ankushyadav9302@gmail.com>
2021-08-27 18:43:30 +0530

    [FEATURE] Add backend to save student information

--- app/Http/Controllers/Student/InfoController.php
+++ app/Http/Controllers/Student/InfoController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Student;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
 
 use App\CustomHelper;
+use App\Http\Requests\StoreStudentInfo;
 use App\Models\Student;
 use App\Models\Semester;
 use App\Models\StudentInfo;
@@ -30,8 +32,25 @@ class InfoController extends Controller {
         ]);
     }
 
-    public function saveNew(Request $request, Student $student_by_roll_number) {
+    public function saveNew(StoreStudentInfo $request, Student $student_by_roll_number) {
         $student = $student_by_roll_number;
         $this->authorize('create', [StudentInfo::class, $student]);
+
+        try {
+            $info = new StudentInfo($request->all());
+            $info->student_id = $student->id;
+            $info->save();
+            Log::info('Student info added.', [Auth::user()]);
+        } catch (\Exception $e) {
+            Log::debug('Failtd to add student info!', [Auth::user(), $e->getMessage(), $student, $request->all()]);
+            return back()->with([
+                'status' => 'fail',
+                'message' => 'An unknown error occurred!'
+            ])->withInput();
+        }
+        return redirect()->route('student.home', $student->roll_number)->with([
+            'status' => 'success',
+            'message' => 'Information added'
+        ]);
     }
 }
