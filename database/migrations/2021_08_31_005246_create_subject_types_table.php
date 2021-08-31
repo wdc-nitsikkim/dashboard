6558cb295f62e345aae1e156b5323228e1e8a006
Ankush Yadav <ankushyadav9302@gmail.com>
2021-08-31 07:09:12 +0530

    [UPDATE] Update existing & add new migration structures related to subjects table

---
+++ database/migrations/2021_08_31_005246_create_subject_types_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubjectTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subject_types', function (Blueprint $table) {
+            $table->tinyIncrements('id');
+            $table->string('name')->nullable(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subject_types');
+    }
+}
